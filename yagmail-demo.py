3d417d41149ea1203d1062bf9418624a3ece5825
KITRI <kitri@a.com>
2019-08-23 17:02:46 +0900

    commit

--- yagmail-demo.py
+++ yagmail-demo.py
@@ -6,16 +6,24 @@ PASS = ''
 
 import smtplib
 from email.mime.text import MIMEText
+from email.header import Header
 
-msg = MIMEText('본문입니다.')
-msg['Subject'] = '제목입니다.'
+unpaid_list = ['soongon@gmail.com', 'soongon@hucloud.co.kr']
 
 smtp = smtplib.SMTP(host='smtp.naver.com', port=587)
 smtp.ehlo()
 smtp.starttls()
 smtp.login(user='ssgoni', password=PASS)
-smtp.sendmail('ssgoni@naver.com', ['soongon@gmail.com'], msg.as_string())
-smtp.close()
+
+for customer in unpaid_list:
+    msg = MIMEText('본문입니다.')
+    msg['Subject'] = Header('제목입니다.', charset='utf-8')
+    msg['From'] = 'ssgoni@naver.com'
+    msg['To'] = customer
+
+    smtp.send_message(msg)
+
+smtp.quit()
 print('mail sended..')
 
 
