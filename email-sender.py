d4d7f65d6e4556ea474b042015442916891b90f1
soongon <soongon@gmail.com>
2019-11-18 13:28:49 +0900

    email

---
+++ email-sender.py
@@ -0,0 +1,63 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+# SMTP 접속을 위한 서버, 계정 설정
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+# 보내는 메일 계정
+SMTP_USER = 'soongon'
+SMTP_PASSWORD = 'tnsrhsl30'
+
+
+# 이메일 유효성 검사 함수
+def is_valid(addr):
+    import re
+    if re.match('(^[a-zA-Z-0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', addr):
+        return True
+    else:
+        return False
+
+
+# 이메일 보내기 함수
+def send_mail(addr, subj_layout, cont_layout, attachment=None):
+    if not is_valid(addr):
+        print("Wrong email: " + addr)
+        return
+
+    # 텍스트 파일
+    msg = MIMEMultipart("alternative")
+    # 첨부파일이 있는 경우 mixed로 multipart 생성
+    if attachment:
+        msg = MIMEMultipart('mixed')
+    msg["From"] = SMTP_USER
+    msg["To"] = addr
+    msg["Subject"] = subj_layout
+    contents = cont_layout
+    text = MIMEText(_text=contents, _charset="utf-8")
+    msg.attach(text)
+    # 첨부파일이 있으면
+    if attachment:
+        from email.mime.base import MIMEBase
+        from email import encoders
+        file_data = MIMEBase("application", "octect-stream")
+        file_data.set_payload(open(attachment, "rb").read())
+        encoders.encode_base64(file_data)
+        import os
+        filename = os.path.basename(attachment)
+        file_data.add_header("Content-Disposition", 'attachment', filename=('UTF-8', '', filename))
+        msg.attach(file_data)
+    # smtp로 접속할 서버 정보를 가진 클래스변수 생성
+    smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    smtp.ehlo()
+    smtp.starttls()
+    # 해당 서버로 로그인
+    smtp.login(SMTP_USER, SMTP_PASSWORD)
+    # 메일 발송
+    smtp.sendmail(SMTP_USER, addr, msg.as_string())
+    # 닫기
+    smtp.close()
+
+
+send_mail('soongon@hucloud.co.kr', '제목입니다.', '본문입니다.')
+print('mail sended..')
